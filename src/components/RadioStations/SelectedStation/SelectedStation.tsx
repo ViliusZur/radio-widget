4f6f5e3ce8a8f268960d22ae20f753554c2d781a
ViliusZur <vilius.zurauskas@gmail.com>
2020-09-28 20:20:58 +0300

    chore: added docker support and deployed widget with netlify

--- src/components/RadioStations/SelectedStation/SelectedStation.tsx
+++ src/components/RadioStations/SelectedStation/SelectedStation.tsx
@@ -7,28 +7,24 @@ import plus from '../../../assets/icons/plus.webp';
 
 // interfaces
 interface Props {
-	station: { name: string; coverImage: string };
+  station: { name: string; coverImage: string };
 }
 
 // displays buttons and an image for an expanded selected station
 const SelectedStation: React.FC<Props> = ({ station }) => (
-	<>
-		<div className={styles.Container}>
-			<div className={styles.Items}>
-				<button className={styles.Invisible}>
-					<img className={styles.PlusMinus} src={minus} alt="minus" />
-				</button>
-				<img
-					className={styles.StationImage}
-					src={station.coverImage}
-					alt={station.name}
-				/>
-				<button className={styles.Invisible}>
-					<img className={styles.PlusMinus} src={plus} alt="plus" />
-				</button>
-			</div>
-		</div>
-	</>
+  <>
+    <div className={styles.Container}>
+      <div className={styles.Items}>
+        <button className={styles.Invisible}>
+          <img className={styles.PlusMinus} src={minus} alt="minus" />
+        </button>
+        <img className={styles.StationImage} src={station.coverImage} alt={station.name} />
+        <button className={styles.Invisible}>
+          <img className={styles.PlusMinus} src={plus} alt="plus" />
+        </button>
+      </div>
+    </div>
+  </>
 );
 
 export default SelectedStation;
--- src/components/RadioWidget/RadioWidget.tsx
+++ src/components/RadioWidget/RadioWidget.tsx
@@ -9,97 +9,44 @@ import CurrentlyPlaying from '../CurrentlyPlaying/CurrentlyPlaying';
 
 // interfaces
 interface Station {
-	key: number;
-	name: string;
-	frequency: number;
-	coverImage: string;
+  key: number;
+  name: string;
+  frequency: number;
+  coverImage: string;
 }
 interface MyState {
-	selectedStation: Station | null;
+  selectedStation: Station | null;
 }
 
+// get stations from imported data
 let stations: Station[] = [...data.stations];
-console.log('stations', stations);
 
-/*
-// list of stations. CHANGE TO JSON
-const stations: Station[] = [
-	{
-		key: 1,
-		name: 'Station One',
-		frequency: 96.3,
-		coverImage: '/stations/one.jpg',
-	},
-	{
-		key: 2,
-		name: 'Station Two',
-		frequency: 98,
-		coverImage: '/stations/two.jpg',
-	},
-	{
-		key: 3,
-		name: 'Station Three',
-		frequency: 89.5,
-		coverImage: '/stations/three.jpg',
-	},
-	{
-		key: 4,
-		name: 'Station Four',
-		frequency: 100.2,
-		coverImage: '/stations/four.jpg',
-	},
-	{
-		key: 5,
-		name: 'Station Five',
-		frequency: 112.5,
-		coverImage: '/stations/one.jpg',
-	},
-	{
-		key: 6,
-		name: 'Station Six',
-		frequency: 95.2,
-		coverImage: '/stations/two.jpg',
-	},
-	{
-		key: 7,
-		name: 'Station Seven',
-		frequency: 87.8,
-		coverImage: '/stations/three.jpg',
-	},
-	{
-		key: 8,
-		name: 'Station Eight',
-		frequency: 99.8,
-		coverImage: '/stations/four.jpg',
-	},
-];
-*/
 // radio widget is the main component, it holds the state of the widget.
 export default class RadioWidget extends React.Component {
-	state: MyState = {
-		selectedStation: null,
-	};
+  state: MyState = {
+    selectedStation: null,
+  };
 
-	selectStation = (keyArr: string[]) => {
-		// updates state with the selected station
+  selectStation = (keyArr: string[]) => {
+    // updates state with the selected station
 
-		const key: number = parseInt(keyArr[0]); // get the key in number type
-		const index: number = stations.findIndex((x) => x.key === key); // find object in array with the key
+    const key: number = parseInt(keyArr[0]); // get the key in number type
+    const index: number = stations.findIndex((x) => x.key === key); // find object in array with the key
 
-		this.setState(() => ({
-			selectedStation: stations[index],
-		}));
-	};
+    this.setState(() => ({
+      selectedStation: stations[index],
+    }));
+  };
 
-	render() {
-		return (
-			<>
-				<div className={styles.Container}>
-					<TitleBar />
-					<RadioStations stations={stations} onChange={this.selectStation} />
-					<CurrentlyPlaying station={this.state.selectedStation} />
-				</div>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <div className={styles.Container}>
+          <TitleBar />
+          <RadioStations stations={stations} onChange={this.selectStation} />
+          <CurrentlyPlaying station={this.state.selectedStation} />
+        </div>
+      </>
+    );
+  }
 }
