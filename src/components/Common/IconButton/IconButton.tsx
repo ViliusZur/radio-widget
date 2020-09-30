ba5b4216250b52f91ba29f5d26ab2347952d9e0a
ViliusZur <vilius.zurauskas@gmail.com>
2020-09-30 21:07:14 +0300

    chore: changed Radio class to a function component that uses hooks, much cleaner code

--- src/components/Common/IconButton/IconButton.tsx
+++ src/components/Common/IconButton/IconButton.tsx
@@ -6,10 +6,10 @@ interface Props {
   alt: string;
 }
 
-const Button: React.FC<Props> = ({ src, alt }) => (
+const IconButton: React.FC<Props> = ({ src, alt }) => (
   <button className={styles.Invisible}>
     <img src={src} alt={alt} className={styles.Image} />
   </button>
 );
 
-export default Button;
+export default IconButton;
--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './radio.module.css';
 import backArrow from '../../assets/icons/back-arrow.webp';
 import switchButton from '../../assets/icons/switch.webp';
@@ -11,60 +11,41 @@ interface Station {
   frequency: number;
   coverImage: string;
 }
-interface PropsType {
+interface Props {
   stations?: Station[];
 }
-interface StateType {
-  selectedStation?: Station;
-}
 
-// radio widget is the main component, it holds the state of the widget.
-export default class RadioWidget extends React.Component<PropsType, StateType> {
-  constructor(props: PropsType) {
-    super(props);
-  }
+const Radio: React.FC<Props> = ({ stations }) => {
+  const [selectedStation, setSelectedStation] = useState({ name: '' });
 
-  state: StateType = {
-    selectedStation: undefined,
-  };
-
-  selectStation = (keyArr: string[]) => {
-    // updates state with the selected station
-    if (this.props.stations) {
+  const selectStation = (keyArr: string[]) => {
+    if (stations) {
       const key: number = parseInt(keyArr[0]); // get the key in number type
-      const index: number = this.props.stations.findIndex((x) => x.key === key); // find object in array with the key
-      const station = this.props.stations[index];
-
-      this.setState(() => ({
-        selectedStation: station,
-      }));
+      const index: number = stations.findIndex((x) => x.key === key); // find object in array with the key
+      const station = stations[index];
+      setSelectedStation(station);
     }
   };
 
-  render() {
-    const allStations = this.props.stations;
-    const station = this.state.selectedStation;
-    return (
-      <div>
-        <div data-testid="titleBar" className={styles.TitleContainer}>
-          <div className={styles.Items}>
-            <IconButton src={backArrow} alt={'back-arrow'} />
-            stations
-            <IconButton src={switchButton} alt="switch" />
-          </div>
-        </div>
-        <Stations stations={allStations} onChange={this.selectStation} />
-        <div data-testid="currentlyPlaying" className={styles.CurrentlyPlaying}>
-          {station && (
-            <>
-              <div data-testid="currentlyPlayingText" className={styles.currentlyPlaying}>
-                currently playing
-              </div>
-              {station.name}
-            </>
-          )}
+  return (
+    <div>
+      <div data-testid="titleBar" className={styles.TitleContainer}>
+        <div className={styles.Items}>
+          <IconButton src={backArrow} alt="back-arrow" />
+          stations
+          <IconButton src={switchButton} alt="switch" />
         </div>
       </div>
-    );
-  }
-}
+      <Stations stations={stations} onChange={selectStation} />
+      <div data-testid="currentlyPlaying" className={styles.CurrentlyPlaying}>
+        {selectedStation && (
+          <div data-testid="currentlyPlayingText" className={styles.currentlyPlaying}>
+            currently playing <span>{selectedStation.name}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Radio;
